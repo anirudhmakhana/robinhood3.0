87e84d9ab368cf5a336fd5a07ff9681448619e93
anirudhkmitl <makhana.anirudh@gmail.com>
2022-04-05 14:38:51 +0700

    [ADDED]: BuyTokens.js

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,33 @@
 //Components
 import Header from "../components/Header";
+import PortfolioChart from "../components/PortfolioChart";
+import BuyTokens from "../components/BuyTokens";
 
 //Icons
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 import { AiOutlinePlus } from "react-icons/ai";
+//Styles
 const styles = {
   wrapper: "w-screen h-screen flex flex-col",
   mainContainer: "w-2/3 h-full m-auto flex mt-16",
-  leftMain: "flex flex-col w-3/4 h-full p-6 overflow-y-scroll",
-  portfolioAmountContainer: "flex flex-col",
+  leftMain: "flex flex-col w-3/4 h-full  p-6 overflow-y-scroll",
+  portfolioAmountContainer: "flex flex-col ",
   portfolioAmount: "text-white text-4xl",
   portfolioPercent: "text-white font-bold text-sm",
   pastHour: "text-gray-400",
   chartContainer:
     "text-5xl flex justify-center w-full h-1/3 text-white mt-11 mb-11",
   buyingPowerContainer:
-    "w-full border-t mb-24 border-b h-16 border-[#30363b] flex justify-between item",
+    "w-full border-t mb-24 border-b h-16 border-[#30363b] flex justify-between items-center p-4",
   buyingPowerTitle: "text-white font-bolder text-lg",
   buyingPowerAmount: "text-white font-bolder text-xl",
-  notice: "flex border border-[#30363b] mx-11 my-4 p-5  flex-col flex-1",
+  notice: "flex border border-[#30363b] mx-11 my-4 p-5 flex-col flex-1",
   noticeContainer: "flex-1",
   noticeTitle: "text-gray-500",
   noticeMessage: "text-white font-bold",
   noticeCTA: "font-bold text-green-500 cursor-pointer mt-5",
   rightMain:
-    "flex flex-col flex-1 h-4/5 bg-[#1E2123] mt-6 rounded-lg overflow-scroll noScroll",
+    "flex flex-col flex-1 h-4/5 bg-[#1E2123] mt-6 rounded-lg overflow-y-scroll noScroll",
   rightMainItem: "flex items-center text-white p-5 border-b border-[#30363b]",
   ItemTitle: "flex-1 font-bold",
   moreOptions: "cursor-pointer text-xl",
@@ -44,7 +47,7 @@ export default function Home() {
           </div>
           <div>
             <div className={styles.chartContainer}>
-              {/* <PortfolioChart/> */}
+              <PortfolioChart />
             </div>
           </div>
           <div className={styles.buyingPowerContainer}>
@@ -57,7 +60,7 @@ export default function Home() {
               <div className={styles.noticeMessage}>
                 Transfer your funds here.
               </div>
-              {/* <BuyTokens/> */}
+              <BuyTokens />
             </div>
           </div>
           {/* <Notice/> */}
