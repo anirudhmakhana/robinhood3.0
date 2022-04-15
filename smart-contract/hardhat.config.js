26d81349ee6a7e759caa96dad7be240ed5ea2ead
anirudhkmitl <makhana.anirudh@gmail.com>
2022-04-15 22:49:38 +0700

    [FINISHED]: Smart contracts

---
+++ smart-contract/hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config({path:__dirname+'/.env'})
+
+const { ALCHEMY_API_URL, WALLET_PRIVATE_KEY } = process.env
+
+
+module.exports = {
+  solidity: '0.8.4',
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_URL,
+      accounts: [
+        WALLET_PRIVATE_KEY],
+    },
+  },
+}
